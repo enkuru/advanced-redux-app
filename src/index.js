93de882ebae788d7d29b2123220afd70bf4659fe
Enes KURU <enes793k@icloud.com>
2018-10-03 15:44:31 +0300

    first commit

--- src/index.js
+++ src/index.js
@@ -4,9 +4,52 @@ import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+import {createStore} from 'redux';
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: http://bit.ly/CRA-PWA
+const initialState = {
+  count: 1,
+  users: []
+};
+
+const reducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'ADD':/*bu şekilde mutable değişken sağlıyoruz, yani memory deki değişken alınır değiştirilir ve yeniden gönderilir, üzerine yazılarak sadece son state durumuna sahip olma problemini giderir*/
+      /*return {
+        ...state,
+        count: state.count + action.payload
+      };*/
+
+      return Object.assign({}, state, {count: state.count + action.payload});/*bu şekilde de kullanılabilir */
+    case 'SUBTRACT':
+      return {
+        ...state,
+        count: state.count - action.payload
+      };
+    default:
+      return state;
+  }
+};
+
+const store = createStore(reducer);
+
+store.subscribe(() => {
+  console.log('store updated!', store.getState());
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 1
+});
+
+store.dispatch({
+  type: 'ADD',
+  payload: 10
+});
+
+store.dispatch({
+  type: 'SUBTRACT',
+  payload: 5
+});
+
+ReactDOM.render(<App/>, document.getElementById('root'));
 serviceWorker.unregister();
